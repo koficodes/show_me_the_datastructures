2c3f64eaca284163716e9df5bba059ffb7701d8d
Paul Karikari <paul.karikari@dashboard.lu>
2019-06-18 03:18:08 +0200

    Add extra test cases to problem 5

--- problem_5.py
+++ problem_5.py
@@ -4,7 +4,9 @@ import time
 
 class Block:
 
-    def __init__(self, timestamp, data, previous_hash=None):
+    def __init__(self, timestamp=None, data=None, previous_hash=None):
+        if not timestamp or not data:
+            raise("Timestamp or Data required")
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
@@ -31,7 +33,9 @@ class BlockChain:
     def __init__(self):
         self.head = None
 
-    def append(self, block):
+    def append(self, block=None):
+        if not block:
+            return
 
         if not self.head:
             self.head = block
@@ -55,6 +59,8 @@ class BlockChain:
         return count
 
     def __str__(self):
+        if self.head is None:
+            return 'Block chain empty'
         node = self.head
         output = ''
         while node:
@@ -70,6 +76,16 @@ blockchain.append(Block(time.time(), '2nd transaction'))
 blockchain.append(Block(time.time(), '3nd transaction'))
 
 print(blockchain)
+
 # 1560701945.9167106-1st transaction-2f274d2e5f3ac3a88f7b90ac0eb52489587d5e0a2fe605e89cc743a818d47b2b-None
 # 1560701945.916737-2nd transaction-0b40d31d754b14138d1efeaeccade1f5631899236901416b91cd7b5d219e6566-2f274d2e5f3ac3a88f7b90ac0eb52489587d5e0a2fe605e89cc743a818d47b2b
 # 1560701945.9167457-3nd transaction-7f5dccb637b4650dbafa275456167c1b796c96a11faabe83efda60593acaf098-0b40d31d754b14138d1efeaeccade1f5631899236901416b91cd7b5d219e6566
+
+empty_block = BlockChain()
+print(empty_block)  # Block chain empty
+
+blockchain_2 = BlockChain()
+blockchain_2.append()
+
+# raises exception raise("Timestamp or Data required")
+blockchain_2.append(Block())

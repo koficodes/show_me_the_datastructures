b550c381103a49ba04777dfbc1a158bf88794953
Paul Karikari <paul.karikari@dashboard.lu>
2019-06-22 12:56:53 +0200

    implement LRU cache with Ordered dict

--- explanation_1.md
+++ explanation_1.md
@@ -1,13 +1,13 @@
 Least Recently Used Cache.
 An LRU cache is a type of cache from which we remove the least recently used entry when the cache memory reaches its limit.
-From the above explanation we need a way to keep ranking order of the entries recently used, so I decided to use a queue to keep track of the ranks.
+From the above explanation we need a way to keep ranking order of the entries recently used, so I decided to use custom class that inherits from OrderedDict.
 
-The main container of the catch items is a dictionary/Hashmap. It is a suitable datastructure
+This is a dictionary/Hashmap. It is a suitable datastructure
 for this problems and it also keeps the operations at least at the order of O(1).
 
 The pop and append operation of the queue are both 0(1)
 All get operations is always O(1). 
-The set operations is mostl 0(1).
+The set operations is always 0(1).
 
 In conclusion, all operation are Order of O(1).
 
--- problem_1.py
+++ problem_1.py
@@ -1,31 +1,27 @@
 import warnings
-from collections import deque
+from collections import OrderedDict
 
 
-class LRU_Cache:
+class LRU_Cache(OrderedDict):
 
-    def __init__(self, capacity=5):
+    def __init__(self, capacity=None):
         # Initialize class variables
         if not capacity:
+            capacity = 5
             warnings.warn("Cache size required, using 5 as default")
         self.capacity = capacity
-        self.access_rank = deque(maxlen=capacity)
-        self.bucket = {}
-
-    def __str__(self):
-        bucket_string = ""
-        for key, item in self.bucket.items():
-            bucket_string += f"\n{item}"
-        return bucket_string
 
     def get(self, key=None):
         # Retrieve item from provided key. Return -1 if nonexistent.
         if not key:
             return -1
 
-        if key in self.bucket:
-            self.access_rank.appendleft(key)
-            return self.bucket[key]
+        try:
+            value = super().__getitem__(key)
+            self.move_to_end(key)
+            return value
+        except KeyError:
+            return -1
 
         return -1
 
@@ -36,11 +32,10 @@ class LRU_Cache:
 
         # Set the value if the key is not present in the cache.
         # If the cache is at capacity remove the oldest item.
-
-        if self.capacity == len(self.bucket):
-            least_used_key = self.access_rank.pop()
-            del self.bucket[least_used_key]
-        self.bucket[key] = value
+        super().__setitem__(key, value)
+        if len(self) > self.capacity:
+            oldest_item = next(iter(self))
+            del self[oldest_item]
 
 
 our_cache = LRU_Cache(5)
@@ -48,10 +43,10 @@ our_cache = LRU_Cache(5)
 our_cache.set(1, 1)
 our_cache.set(2, 2)
 our_cache.set()
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(3)       # return -1
-our_cache.get()        # return -1
+print(our_cache.get(1))     # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(3))       # return -1
+print(our_cache.get())        # return -1
 
 
 our_cache = LRU_Cache(4)
@@ -61,14 +56,29 @@ our_cache.set(2, 2)
 our_cache.set(3, 3)
 
 
-our_cache.get(1)       # returns 1
-our_cache.get(2)       # returns 2
-our_cache.get(9)      # returns -1 because 9 is not present in the cache
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5)
 our_cache.set(6, 6)
 
-our_cache.get(3)  # returns -1
+print(our_cache.get(3))  # returns -1
 
 
 our_cache = LRU_Cache()  # gives warning
+print(our_cache.get(1))  # return -1
+our_cache.set(2, 2)
+print(our_cache.get(2))  # 2
+
+# output
+# 1
+# 2
+# -1
+# -1
+# 1
+# 2
+# -1
+# -1
+# -1
+# 2
